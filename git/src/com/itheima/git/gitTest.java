fa16e5c1fd2eb7ef4629b11d98c9d0efbbce547f
tGuo_global <gitTest_global@163.com>
2021-01-19 21:13:32 +0800

    首次测试idea的git

---
+++ git/src/com/itheima/git/gitTest.java
@@ -0,0 +1,9 @@
+package com.itheima.git;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("gitTest");
+
+        System.out.println("gitTest2");
+    }
+}
