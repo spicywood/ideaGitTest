53d0296cef4a8e912b05a024b8c961ac90c8dec0
tGuo_global <gitTest_global@163.com>
2021-01-28 20:44:27 +0800

    测试冲突2-2

--- git/src/com/itheima/git/gitTest.java
+++ git/src/com/itheima/git/gitTest.java
@@ -7,15 +7,19 @@ public class gitTest {
         System.out.println("gitTest2");
     }
 
-    public void show(){
+    public void show() {
         System.out.println("gitTest3 new insert");
     }
 
-    public void show2(){
+    public void show2() {
         System.out.println("gitTest4 modify insert");
     }
 
-    public void show3(){
-        System.out.println("gitTest5 conflict2");
+
+
+
+
+    public void show4() {
+        System.out.println("gitTest4 branches test");
     }
 }
